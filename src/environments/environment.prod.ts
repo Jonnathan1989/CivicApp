daec56e8f4e3c5aca082dfbeb2782352d6429575
Jonnathan1989 <jonnathanreys@gmail.com>
2026-05-30 17:00:55 -0500

    Avance CivicApp - pantallas implementadas y Capacitor configurado

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,11 @@
 export const environment = {
-  production: true
+  production: true,
+  firebase: {
+    apiKey: "AIzaSyD-y5C4Aqxvw3fsqfiyfmz767bF7dJhOxE",
+    authDomain: "civicapp-65df3.firebaseapp.com",
+    projectId: "civicapp-65df3",
+    storageBucket: "civicapp-65df3.firebasestorage.app",
+    messagingSenderId: "434216188594",
+    appId: "1:434216188594:web:ec189d4b608aeb1e0aa7ab"
+  }
 };
